b44122dc9baa1d0851b3ab0388e56db2dfa0464b
Kuzj <kuzj99@gmail.com>
2019-03-14 22:06:15 +0300

    first realization

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
-
-import socket
 import sys
+import asyncio
 import random
-import time
-from threading import Thread
 
-def doubler_server(port = 8080):
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.bind(('', port))
-		s.listen(5)
-		s.setblocking(False)
-		while True:
-			try:
-				conn, addr = s.accept()
-			except BlockingIOError:
-				continue
-			t = Thread(target = handle_connection, args = (conn, addr))
-			t.start()
+async def doubler_server(host = '127.0.0.1', port = 8080):
+	server = await asyncio.start_server(
+		handle_connection, host, port)
+	addr = server.sockets[0].getsockname()
+	async with server:
+		await server.serve_forever()
 
-def handle_connection(conn, addr):
+async def handle_connection(reader, writer):
+	data = 'dummy'
+	addr = writer.get_extra_info('peername')
 	print('Connected by', addr)
-	with conn:
-		while True:
-			data = conn.recv(1024)
-			if not data:
-				break
-			n = int(data.decode())
-			res = f"{n*2}\n".encode()
-			conn.send(res)
-		print('Disconnected by', addr)
+	while True:
+		data = await reader.read(100)
+		if not data:
+			break
+		res = int(data.decode())*2
+		writer.write(f"{res}".encode())
+		await writer.drain()
+	print('Disconnected by', addr)
+	writer.close()
 
-def doubler_client(port = 8080):
-	with socket.create_connection(("127.0.0.1", port)) as s:
-		f = s.makefile(mode="rw", buffering=1, newline="\n")
-		while True:
-			n = random.randrange(10)
-			f.write(f"{n}\n")
-			print(n, f.readline().strip())
-			time.sleep(random.random()*2)
+async def doubler_client(host = '127.0.0.1', port = 8080):
+	reader, writer = await asyncio.open_connection(
+		host, port)
+	while True:
+		n = random.randrange(10)
+		writer.write(f"{n}\n".encode())
+		data = await reader.read(100)
+		print(n, data.decode())
+		await asyncio.sleep(random.random()*2)
 
 if __name__ == '__main__':
 	if sys.argv[1] == 'server':
-		doubler_server()
+		asyncio.run(doubler_server())
 	else:
 		assert sys.argv[1] == 'client'
-		doubler_client()
+		asyncio.run(doubler_client())
